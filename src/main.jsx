fe2901b72aafbe142ae45771188be350aaa1c7b2
desertrecsportsleague-star <desertrecsportsleague@gmail.com>
2026-08-17 19:38:28 -0700

    Enable dynamic Jarvis homepage media

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 import App from './App.jsx'
 import JarvisPanel from './JarvisPanel.jsx'
 import JarvisAdminTools from './JarvisAdminTools.jsx'
+import JarvisHomepageSync from './JarvisHomepageSync.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <JarvisPanel />
     <JarvisAdminTools />
+    <JarvisHomepageSync />
   </StrictMode>,
 )
