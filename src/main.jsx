923c6df139514f0d21cdac630f0ce20a46213a39
desertrecsportsleague-star <desertrecsportsleague@gmail.com>
2026-08-17 19:46:36 -0700

    Remove public Jarvis mount

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import JarvisPanel from './JarvisPanel.jsx'
 import JarvisAdminTools from './JarvisAdminTools.jsx'
 import JarvisHomepageSync from './JarvisHomepageSync.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <JarvisPanel />
     <JarvisAdminTools />
     <JarvisHomepageSync />
   </StrictMode>,
