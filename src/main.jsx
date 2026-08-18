24109af36da53c5e384d6e10edb4b06afb39515b
desertrecsportsleague-star <desertrecsportsleague@gmail.com>
2026-08-17 19:13:06 -0700

    Mount combined Jarvis managers

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import './index.css'
 import App from './App.jsx'
 import JarvisPanel from './JarvisPanel.jsx'
 import JarvisWebsiteManager from './JarvisWebsiteManager.jsx'
+import JarvisTournamentManager from './JarvisTournamentManager.jsx'
+import JarvisScheduleManager from './JarvisScheduleManager.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -11,6 +13,8 @@ createRoot(document.getElementById('root')).render(
     <JarvisPanel />
     <div style={{maxWidth: '1120px', margin: '0 auto', padding: '0 16px 40px'}}>
       <JarvisWebsiteManager />
+      <JarvisTournamentManager />
+      <JarvisScheduleManager />
     </div>
   </StrictMode>,
 )
