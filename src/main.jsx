6baebd2a23a1513c108f648416b1618320123478
desertrecsportsleague-star <desertrecsportsleague@gmail.com>
2026-08-17 17:27:43 -0700

    Wire Jarvis website manager into preview

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import JarvisPanel from './JarvisPanel.jsx'
+import JarvisWebsiteManager from './JarvisWebsiteManager.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <JarvisPanel />
+    <div style={{maxWidth: '1120px', margin: '0 auto', padding: '0 16px 40px'}}>
+      <JarvisWebsiteManager />
+    </div>
   </StrictMode>,
 )
