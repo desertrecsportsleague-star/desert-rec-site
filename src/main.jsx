d22f8963be0ea8806f85f62a35751023f146e3ea
desertrecsportsleague-star <desertrecsportsleague@gmail.com>
2026-08-17 19:28:21 -0700

    Restrict Jarvis management modules to admin dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import JarvisPanel from './JarvisPanel.jsx'
-import JarvisWebsiteManager from './JarvisWebsiteManager.jsx'
-import JarvisTournamentManager from './JarvisTournamentManager.jsx'
-import JarvisScheduleManager from './JarvisScheduleManager.jsx'
+import JarvisAdminTools from './JarvisAdminTools.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <JarvisPanel />
-    <div style={{maxWidth: '1120px', margin: '0 auto', padding: '0 16px 40px'}}>
-      <JarvisWebsiteManager />
-      <JarvisTournamentManager />
-      <JarvisScheduleManager />
-    </div>
+    <JarvisAdminTools />
   </StrictMode>,
 )
