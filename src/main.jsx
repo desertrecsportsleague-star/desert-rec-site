a2630ab142a0b8575612268194dd3875120a40b7
desertrecsportsleague-star <desertrecsportsleague@gmail.com>
2026-09-02 19:00:46 -0700

    Wire Fall softball registration and waiver finder

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,18 @@ import App from './App.jsx'
 import JarvisAdminTools from './JarvisAdminTools.jsx'
 import JarvisHomepageSync from './JarvisHomepageSync.jsx'
 import { TournamentPublic, TournamentRoster } from './TournamentPublic.jsx'
+import FallSoftballRegistration from './FallSoftballRegistration.jsx'
 
 const params = new URLSearchParams(window.location.search)
 const isTournament = window.location.pathname === '/tournaments' || params.has('tournament')
 const isRoster = window.location.pathname === '/tournament-roster' || params.has('roster')
+const isFallSoftball = window.location.pathname === '/fall-softball'
+const special = isTournament || isRoster || isFallSoftball
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {isRoster ? <TournamentRoster /> : isTournament ? <TournamentPublic /> : <App />}
-    {!isTournament && !isRoster && <JarvisAdminTools />}
-    {!isTournament && !isRoster && <JarvisHomepageSync />}
+    {isRoster ? <TournamentRoster /> : isTournament ? <TournamentPublic /> : isFallSoftball ? <FallSoftballRegistration /> : <App />}
+    {!special && <JarvisAdminTools />}
+    {!special && <JarvisHomepageSync />}
   </StrictMode>,
 )
