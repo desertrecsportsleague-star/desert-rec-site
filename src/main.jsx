443514b5f24d7fcd5552ba8617d4c119949e0322
desertrecsportsleague-star <desertrecsportsleague@gmail.com>
2026-09-02 19:19:05 -0700

    Mount Fall registration navigation sync

--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,12 @@ import './index.css'
 import App from './App.jsx'
 import JarvisAdminTools from './JarvisAdminTools.jsx'
 import JarvisHomepageSync from './JarvisHomepageSync.jsx'
+import JarvisRegistrationSync from './JarvisRegistrationSync.jsx'
 import { TournamentPublic, TournamentRoster } from './TournamentPublic.jsx'
 import FallSoftballRegistration from './FallSoftballRegistration.jsx'
-
-const params = new URLSearchParams(window.location.search)
-const isTournament = window.location.pathname === '/tournaments' || params.has('tournament')
-const isRoster = window.location.pathname === '/tournament-roster' || params.has('roster')
-const isFallSoftball = window.location.pathname === '/fall-softball'
-const special = isTournament || isRoster || isFallSoftball
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    {isRoster ? <TournamentRoster /> : isTournament ? <TournamentPublic /> : isFallSoftball ? <FallSoftballRegistration /> : <App />}
-    {!special && <JarvisAdminTools />}
-    {!special && <JarvisHomepageSync />}
-  </StrictMode>,
-)
+const params=new URLSearchParams(window.location.search)
+const isTournament=window.location.pathname==='/tournaments'||params.has('tournament')
+const isRoster=window.location.pathname==='/tournament-roster'||params.has('roster')
+const isFallSoftball=window.location.pathname==='/fall-softball'
+const special=isTournament||isRoster||isFallSoftball
+createRoot(document.getElementById('root')).render(<StrictMode>{isRoster?<TournamentRoster/>:isTournament?<TournamentPublic/>:isFallSoftball?<FallSoftballRegistration/>:<App/>}{!special&&<JarvisAdminTools/>}{!special&&<JarvisHomepageSync/>}{!special&&<JarvisRegistrationSync/>}</StrictMode>)
