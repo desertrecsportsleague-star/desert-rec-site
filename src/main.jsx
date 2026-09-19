a6815c137160e00fae311c56eb5dc27e2306a7ca
desertrecsportsleague-star <desertrecsportsleague@gmail.com>
2026-09-19 15:16:32 -0700

    Repair malformed Fall schedule route source

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,27 @@ import JarvisHomepageSync from './JarvisHomepageSync.jsx'
 import JarvisRegistrationSync from './JarvisRegistrationSync.jsx'
 import { TournamentPublic, TournamentRoster } from './TournamentPublic.jsx'
 import FallSoftballRegistration from './FallSoftballRegistration.jsx'
-import FallCaptainRoster from './FallCaptainRoster.jsx'\nimport FallSoftballSchedule from './FallSoftballSchedule.jsx'
-const params=new URLSearchParams(window.location.search)
-const isTournament=window.location.pathname==='/tournaments'||params.has('tournament')
-const isRoster=window.location.pathname==='/tournament-roster'||params.has('roster')
-const isFallSoftball=window.location.pathname==='/fall-softball'
-const isFallCaptain=window.location.pathname==='/fall-softball-captain'\nconst isFallSchedule=window.location.pathname==='/fall-softball-schedule'
-const special=isTournament||isRoster||isFallSoftball||isFallCaptain||isFallSchedule
-createRoot(document.getElementById('root')).render(<StrictMode>{isRoster?<TournamentRoster/>:isTournament?<TournamentPublic/>:isFallSchedule?<FallSoftballSchedule/>:isFallCaptain?<FallCaptainRoster/>:isFallSoftball?<FallSoftballRegistration/>:<App/>}{!special&&<JarvisAdminTools/>}{!special&&<JarvisHomepageSync/>}{!special&&<JarvisRegistrationSync/>}</StrictMode>)
+import FallCaptainRoster from './FallCaptainRoster.jsx'
+import FallSoftballSchedule from './FallSoftballSchedule.jsx'
+
+const params = new URLSearchParams(window.location.search)
+const isTournament = window.location.pathname === '/tournaments' || params.has('tournament')
+const isRoster = window.location.pathname === '/tournament-roster' || params.has('roster')
+const isFallSoftball = window.location.pathname === '/fall-softball'
+const isFallCaptain = window.location.pathname === '/fall-softball-captain'
+const isFallSchedule = window.location.pathname === '/fall-softball-schedule'
+const special = isTournament || isRoster || isFallSoftball || isFallCaptain || isFallSchedule
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {isRoster ? <TournamentRoster /> :
+      isTournament ? <TournamentPublic /> :
+      isFallSchedule ? <FallSoftballSchedule /> :
+      isFallCaptain ? <FallCaptainRoster /> :
+      isFallSoftball ? <FallSoftballRegistration /> :
+      <App />}
+    {!special && <JarvisAdminTools />}
+    {!special && <JarvisHomepageSync />}
+    {!special && <JarvisRegistrationSync />}
+  </StrictMode>
+)
