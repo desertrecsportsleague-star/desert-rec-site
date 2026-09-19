a64ab6a24d9cdfba0786c4ed144b1780cd3052ab
desertrecsportsleague-star <desertrecsportsleague@gmail.com>
2026-09-19 14:29:44 -0700

    Route Fall softball schedule page

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,11 @@ import JarvisHomepageSync from './JarvisHomepageSync.jsx'
 import JarvisRegistrationSync from './JarvisRegistrationSync.jsx'
 import { TournamentPublic, TournamentRoster } from './TournamentPublic.jsx'
 import FallSoftballRegistration from './FallSoftballRegistration.jsx'
-import FallCaptainRoster from './FallCaptainRoster.jsx'
+import FallCaptainRoster from './FallCaptainRoster.jsx'\nimport FallSoftballSchedule from './FallSoftballSchedule.jsx'
 const params=new URLSearchParams(window.location.search)
 const isTournament=window.location.pathname==='/tournaments'||params.has('tournament')
 const isRoster=window.location.pathname==='/tournament-roster'||params.has('roster')
 const isFallSoftball=window.location.pathname==='/fall-softball'
-const isFallCaptain=window.location.pathname==='/fall-softball-captain'
-const special=isTournament||isRoster||isFallSoftball||isFallCaptain
-createRoot(document.getElementById('root')).render(<StrictMode>{isRoster?<TournamentRoster/>:isTournament?<TournamentPublic/>:isFallCaptain?<FallCaptainRoster/>:isFallSoftball?<FallSoftballRegistration/>:<App/>}{!special&&<JarvisAdminTools/>}{!special&&<JarvisHomepageSync/>}{!special&&<JarvisRegistrationSync/>}</StrictMode>)
+const isFallCaptain=window.location.pathname==='/fall-softball-captain'\nconst isFallSchedule=window.location.pathname==='/fall-softball-schedule'
+const special=isTournament||isRoster||isFallSoftball||isFallCaptain||isFallSchedule
+createRoot(document.getElementById('root')).render(<StrictMode>{isRoster?<TournamentRoster/>:isTournament?<TournamentPublic/>:isFallSchedule?<FallSoftballSchedule/>:isFallCaptain?<FallCaptainRoster/>:isFallSoftball?<FallSoftballRegistration/>:<App/>}{!special&&<JarvisAdminTools/>}{!special&&<JarvisHomepageSync/>}{!special&&<JarvisRegistrationSync/>}</StrictMode>)
